1e7068c16af4799128fec7741e81505b711bdc17
elhordev <diego.elhor@gmail.com>
2023-04-05 13:42:18 +0200

    Solved bugs and Added Realtime for Wallet

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 import os
 import requests
 
-precio_valor_en_wallet = []
-valor_en_wallet = []
 url = "https://www.bolsamania.com/indice/IBEX-35"
+wallet_total = []
+
 
 def borrado_dep_so():
     borrado = None
@@ -32,34 +32,46 @@ def scrapurl(result,acciones,precio_acciones,tiempo_acciones,var_acciones):
     for acc in acc_scrap:
         acciones.append(acc.contents[1].next)
     for price in price_scrap:
-        precio_acciones.append(price.contents[1].next)
+        price = price.contents[1].contents[0].replace("€","")
+        price = price.replace(",",".")
+        precio_acciones.append(float(price))
+
     for time in time_scrap:
         tiempo_acciones.append(time.contents[1].next)
     for var in var_scrap:
         var_acciones.append(var.contents[1].text)
+
     
-    
-def ad_to_wallet(acciones,valor_en_wallet,precio_valor_en_wallet):
+def ad_to_wallet(acciones,wallet_total):
     df_acciones = pd.DataFrame(acciones)
     print(df_acciones)
+    
 
     opcion = int(input("Qué valor del Ibex 35 has comprado?\n[Q]Para salir."))
     if opcion == "Q":
         main()
     else:
-        valor_en_wallet.append(acciones[opcion])
-        opcion1 = float(input("A qué precio has comprado?"))
-        precio_valor_en_wallet.append(opcion1)
-        numero_de_acciones = int(input("Cuantas acciones has comprado?"))
-        opcion2 = float(input("Cuánto te han cobrado en gastos de compra?"))
-        neto_en_cuenta = (opcion1 * numero_de_acciones - opcion2)
-        print(f"Añadida la Compra de {opcion1} acciones del {acciones[opcion]} por un valor de cargo en cuenta de {neto_en_cuenta}€.")
-        
-    return valor_en_wallet
+        Stock = acciones[opcion]
+        Buyprice = float(input(f"A que precio has comprado las acciones de {Stock} ?\n"))
+        Qty = int(input(f"Cuantas acciones de {Stock} has comrpado a {Buyprice}?\n"))
+        Expense = float(input("Cuanto te han cobrado de gastos de compra?"))
+        Index = opcion
+        wallet = dict(
+            Stock = Stock,
+            Buyprice = Buyprice,
+            Qty = Qty,
+            Expense = Expense,
+            Index = Index,
+            AccountCharge = (Buyprice * Qty) + Expense,
+            Balance = 0
+        )
+        print(f"Añadida la compra de {Qty} acciones de {Stock} por un cargo en cuenta de {wallet['AccountCharge']} euros.")
+        wallet_total.append(wallet)
+    return wallet_total
     
 
 def show_tiempo_real(acciones,precio_acciones,tiempo_acciones,var_acciones,
-                     valor_en_wallet,precio_valor_en_wallet):
+                     wallet_total):
     borrado = borrado_dep_so()
     while True:
         acciones = []
@@ -72,8 +84,14 @@ def show_tiempo_real(acciones,precio_acciones,tiempo_acciones,var_acciones,
         df = pd.DataFrame(list(zip(acciones,precio_acciones,var_acciones,tiempo_acciones)),
                           columns=["Valor","Precio","","Hora"])
         print(df)
-        if valor_en_wallet:
-            df1 = pd.DataFrame(valor_en_wallet)
+        if wallet_total:
+            for x in wallet_total:
+                 for y in x:   
+                    if y == "Balance":
+                        x["Balance"] = (precio_acciones[x["Index"]] * x["Qty"]) - x["AccountCharge"]
+
+
+            df1 = pd.DataFrame(wallet_total)
             print(df1)
         sleep(5)
 
@@ -92,16 +110,16 @@ def main():
         precio_acciones = []
         tiempo_acciones = []
         var_acciones = []
-        precio_valor_en_wallet = []
+        
         result = urlcontent(url)
         scrapurl(result,acciones,precio_acciones,tiempo_acciones,var_acciones)
         opcion = menu_principal()
         if opcion == "A":
-            ad_to_wallet(acciones,valor_en_wallet,precio_valor_en_wallet)
+            ad_to_wallet(acciones,wallet_total)
         if opcion == "B":
             print("miau")
         if opcion == "C":
-            show_tiempo_real(acciones,precio_acciones,tiempo_acciones,var_acciones,valor_en_wallet,precio_valor_en_wallet)
+            show_tiempo_real(acciones,precio_acciones,tiempo_acciones,var_acciones,wallet_total)
     
 
 if __name__ == "__main__":
